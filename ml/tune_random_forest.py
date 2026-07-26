11b4aa09b786b3f7862ccce58f78a8eade54e933
Harshpreet Singh <harshpreet170607@gmail.com>
2026-07-26 13:16:40 +0530

    tuning in random forest

---
+++ ml/tune_random_forest.py
@@ -0,0 +1,22 @@
+def load_preprocessed_data():
+    pass
+
+
+def create_param_grid():
+    pass
+
+
+def tune_model():
+    pass
+
+
+def save_model():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
