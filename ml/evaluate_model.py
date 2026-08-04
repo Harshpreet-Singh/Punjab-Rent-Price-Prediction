5c72e54a6a7678c49851d4b69cbd5cd8f8b31115
Harshpreet Singh <harshpreet170607@gmail.com>
2026-08-04 23:50:25 +0530

    subscript fixed

--- ml/evaluate_model.py
+++ ml/evaluate_model.py
@@ -73,7 +73,7 @@ def save_metrics(metrics, model_name, verbose=True):
 
     metrics_path = METRICS_DIR / f"{model_name}_metrics.txt"
 
-    with open(metrics_path, "w") as file:
+    with open(metrics_path, "w", encoding="utf-8") as file:
 
         file.write(f"{model_name_to_title(model_name)} Evaluation\n")
         file.write("=" * 40 + "\n\n")
